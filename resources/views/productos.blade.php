3bb0e3616fad8ae4d5a89074d4f2d0352b1f50eb
alumno <alumno@vm1>
2021-12-01 20:10:16 +0100

    Ejercicios bases de datos

--- resources/views/productos.blade.php
+++ resources/views/productos.blade.php
@@ -5,18 +5,15 @@
 <div class="row">
 
     @php
-    $key = 0;
     @endphp
+
     @foreach( $arrayProductos as $producto )
-        @php
-            $key++;
-        @endphp
     <div class="col-xs-6 col-sm-4 col-md-3 text-center">
 
-        <a href="{{ url('/productos/show/' . $key ) }}">
+        <a href="{{ url('/productos/show/' . $producto->id ) }}">
             <img src="https://picsum.photos/200/300/?random" style="height:200px"/>
             <h4 style="min-height:45px;margin:5px 0 10px 0">
-                {{$producto[0]}}
+                {{$producto->nombre}}
             </h4>
         </a>
 
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -12,17 +12,35 @@
     </div>
     <div class="col-sm-8">
         <h1 style="min-height:45px;margin:5px 0 10px 0">
-            {{$producto[0]}}
+            {{$producto->nombre}}
         </h1>
         <h4 style="min-height:45px;margin:5px 0 10px 0">
-            {{$producto[1]}}
+            {{$producto->categoria}}
         </h4>
         <p>
-            Producto actualmente comprado
+            @php
+                if ($producto->pendiente){
+                    echo "Producto actualmente comprado";
+                } else {
+                    echo "Producto actualmente en venta";
+                }
+            @endphp
         </p>
-        <a type="submit" class="btn btn-danger" role="button" href="#">Pendiente de compra</a>
+        <a type="submit" class="btn btn-@php if ($producto->pendiente){
+            echo "danger";
+        } else {
+            echo "success";
+        } @endphp" role="button" href="#">
+            @php
+                if ($producto->pendiente){
+                    echo "Comprado";
+                } else {
+                    echo "Comprar";
+                }
+            @endphp
+        </a>
         <br><br>
-        <a type="submit" class="btn btn-default" role="button" href="{{ url('productos/edit/'.$id) }}">Editar</a>
+        <a type="submit" class="btn btn-default" role="button" href="{{ url('productos/edit/'.$producto->id) }}">Editar</a>
         <br><br>
         <a type="submit" class="btn btn-default" role="button" href="{{ url('productos') }}">Volver al listado</a>
 
